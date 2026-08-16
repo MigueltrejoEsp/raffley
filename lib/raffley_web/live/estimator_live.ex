50f0a3532020b01633edbf81d43e95318c49e990
MigueltrejoEsp <mangeel008@gmail.com`>
2026-08-16 15:07:51 -0300

    handle event

--- lib/raffley_web/live/estimator_live.ex
+++ lib/raffley_web/live/estimator_live.ex
@@ -3,6 +3,7 @@ defmodule RaffleyWeb.EstimatorLive do
 
   def mount(_params, _session, socket) do
     socket = assign(socket, tickets: 0, price: 3)
+    IO.inspect(socket)
     {:ok, socket}
   end
 
@@ -12,6 +13,7 @@ defmodule RaffleyWeb.EstimatorLive do
       <h1>Raffle Estimator</h1>
 
       <section>
+        <button phx-click="add" phx-value-quantity="5">+</button>
         <div>
           {@tickets}
         </div>
@@ -26,5 +28,11 @@ defmodule RaffleyWeb.EstimatorLive do
       </section>
     </div>
     """
+  end
+
+  def handle_event("add", %{"quantity" => quantity}, socket) do
+    socket = update(socket, :tickets, &(&1 + String.to_integer(quantity)))
+    IO.inspect(socket)
+    {:noreply, socket}
   end
 end
